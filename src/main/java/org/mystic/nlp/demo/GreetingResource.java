666f14850235db69e2237caa349780e6fc5ea5e6
Konstantin Perikov <pericov.ko@gmail.com>
2022-02-01 16:02:22 +0000

    migrate to quarkus, add simple endpoint with chunker

--- src/main/java/org/mystic/nlp/demo/GreetingResource.java
+++ src/main/java/org/mystic/nlp/demo/GreetingResource.java
@@ -1,16 +1,24 @@
-package org.mystic.opennlp.demo;
+package org.mystic.nlp.demo;
 
 import opennlp.tools.chunker.ChunkerME;
 import opennlp.tools.chunker.ChunkerModel;
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSTaggerME;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() throws IOException {
         InputStream chunkerModelIn = new FileInputStream("models/en-chunker.bin");
         ChunkerModel chunkerModel = new ChunkerModel(chunkerModelIn);
         ChunkerME chunker = new ChunkerME(chunkerModel);
@@ -19,12 +27,16 @@ public class Main {
         POSModel posModel = new POSModel(posTaggerIn);
         POSTaggerME tagger = new POSTaggerME(posModel);
 
-        String sent[] = new String[] { "What", "is", "outcome", "of", "Bernie", "Madoff", "and", "his", "Ponzi", "Scheme" };
+        String sent[] = new String[]{"What", "is", "outcome", "of", "Bernie", "Madoff", "and", "his", "Ponzi", "Scheme"};
 
         String tags[] = tagger.tag(sent);
         String chunkTags[] = chunker.chunk(sent, tags);
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < chunkTags.length; ++i) {
-            System.out.println(sent[i] + " " + tags[i] + " " + chunkTags[i]);
+            result.append(sent[i] + " " + tags[i] + " " + chunkTags[i]);
+            result.append("\n");
         }
+
+        return result.toString();
     }
 }
